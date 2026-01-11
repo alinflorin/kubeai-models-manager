485f02a79e40cf1192cc36c3772b2b9aba1a3ee8
Alin Florin <alinflorinciu@icloud.com>
2026-01-11 14:19:11 +0200

    asd

--- src/kmm-api/src/index.ts
+++ src/kmm-api/src/index.ts
@@ -3,19 +3,30 @@ import express from "express";
 import { version } from "./version";
 import errorHandler from "./middleware/error-handler";
 import notFoundHandler from "./middleware/not-found-handler";
+import * as k8s from "@kubernetes/client-node";
+
+const kc = new k8s.KubeConfig();
+
+if (process.env.NODE_ENV === "production") {
+  kc.loadFromCluster();
+} else {
+  kc.loadFromFile(process.env.HOME + "/.kube/config");
+}
+
+const coreApi = kc.makeApiClient(k8s.CoreV1Api);
+const customApi = kc.makeApiClient(k8s.CustomObjectsApi);
 
 const app = express();
-app.use(express.json());  ///  req.body 
+app.use(express.json()); ///  req.body
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "OK", version: version });
 });
 
-
-
-
-
-
+app.get("/api/namespaces", async (req, res) => {
+  const ns = await coreApi.listNamespace();
+  res.send(ns);
+});
 
 app.use(notFoundHandler);
 
