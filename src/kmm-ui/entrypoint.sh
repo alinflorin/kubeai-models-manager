32f0af6b7821c150826575f56aef3e16e01850cc
Alin Florin <alinflorinciu@icloud.com>
2026-01-13 10:47:02 +0200

    test

--- src/kmm-ui/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Generate runtime env file
-envsubst < /usr/share/caddy/env-config.js.template > /usr/share/caddy/env-config.js
-
-# Start Caddy
-exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
