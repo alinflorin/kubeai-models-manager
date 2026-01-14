9c6942486b6f4d92e825cd5e59f5ccc980114515
Alin Florin <alinflorinciu@icloud.com>
2026-01-14 15:22:36 +0200

    dfg

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,19 @@
+import { makeStyles, shorthands, Title1 } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    ...shorthands.gap("1rem"),
+  },
+});
+
 function Home() {
+    const styles = useStyles();
+
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className={styles.container}>
+            <Title1 as="h1">Home</Title1>
             <p>Welcome to the home page!</p>
         </div>
     );
